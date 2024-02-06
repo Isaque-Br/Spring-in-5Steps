b0a0d014df0d5576221d1610819d1f0432743411
Isaque-Br <izakesz@gmail.com>
2024-02-06 12:55:57 -0300

    Creating class and implementing Bubble Sort, a sorting algorithm that can be applied to Arrays and Dynamic Lists

---
+++ src/main/java/com/basics/springin5steps/BinarySearchImpl.java
@@ -0,0 +1,21 @@
+package com.basics.springin5steps;
+
+public class BinarySearchImpl {
+
+    public int binarySearch(int[] numbers, int numberToSearchFor) {
+        //Sorting an array
+        BubbleSortAlgorithm bubbleSortAlgorithm = new BubbleSortAlgorithm();
+        int[] sortedNumbers = bubbleSortAlgorithm.sort(numbers);
+    //Implementing Sorting Logic
+
+
+
+    // Bublle Sort Algorithm
+    // Search the array
+    //Return the result
+
+        return 3;
+
+    }
+
+}
